27e47e78630651238e53a5ca4ba81eac0c07ed5b
Mariusz Wisniewski <lifcio@gmail.com>
2015-09-08 20:11:20 +0200

    View controller updates

--- youtube-app-syncano/ViewController.swift
+++ youtube-app-syncano/ViewController.swift
@@ -11,51 +11,78 @@ import MediaPlayer
 
 let instanceName = ""
 let apiKey = ""
+let channelName = "youtube_videos"
 
 class ViewController: UIViewController {
     
     var videos : [Video] = []
     let syncano = Syncano.sharedInstanceWithApiKey(apiKey, instanceName: instanceName)
-
+    let channel = SCChannel(name: channelName)
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func refreshPressed(sender: UIBarButtonItem) {
+        self.downloadVideosFromSyncano()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.channel.delegate = self
         self.downloadVideosFromSyncano()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
+
+//MARK - Syncano
+extension ViewController {
     func downloadVideosFromSyncano() {
-        Video.please().giveMeDataObjectsWithCompletion { youtubeVideos, error in
-            if (youtubeVideos == nil) {
-                return;
-            }
-            self.videos.removeAll(keepCapacity: true)
-            for item in youtubeVideos {
-                if let video = item as? Video {
-                    self.videos.append(video)
+        Video.please().giveMeDataObjectsWithCompletion { objects, error in
+            if let youtubeVideos = objects {
+                self.videos.removeAll(keepCapacity: true)
+                for item in youtubeVideos {
+                    if let video = item as? Video {
+                        self.videos.append(video)
+                    }
                 }
+                self.tableView.reloadData()
             }
-            self.tableView.reloadData()
         }
     }
 }
 
+//MARK - SCChannelDelegate
+extension ViewController : SCChannelDelegate {
+    func chanellDidReceivedNotificationMessage(notificationMessage: SCChannelNotificationMessage!) {
+        switch(notificationMessage.action) {
+        case .Create:
+            //TODO Create video from dictionary and add to the list
+            break
+        case .Delete:
+            break
+        case .Update:
+            break
+        default:
+            break
+        }
+    }
+}
+
+//MARK - UITableViewDelegate
 extension ViewController: UITableViewDelegate {
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        if let sourceId = self.videos[indexPath.row].source_id {
+        if let sourceId = self.videos[indexPath.row].sourceId {
             let videoPlayerViewController = XCDYouTubeVideoPlayerViewController(videoIdentifier: sourceId)
             self.presentMoviePlayerViewControllerAnimated(videoPlayerViewController)
         }
     }
 }
 
+//MARK - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.videos.count
@@ -70,7 +97,7 @@ extension ViewController: UITableViewDataSource {
         }
         
         cell?.textLabel?.text = videos[indexPath.row].title
-        cell?.detailTextLabel?.text = videos[indexPath.row].video_description
+        cell?.detailTextLabel?.text = videos[indexPath.row].videoDescription
         
         return cell!
     }
